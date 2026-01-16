a20bb425dd4a67b05e97caa89ea510b0f0bd3192
El_Nico <g.nabiulin@yandex.ru>
2026-01-16 22:07:23 +0300

    Navigation Burger

--- GameCatalog/app/src/main/java/com/mirea/nabiulingb/gamecatalog/presentation/ui/MainActivity.java
+++ GameCatalog/app/src/main/java/com/mirea/nabiulingb/gamecatalog/presentation/ui/MainActivity.java
@@ -2,41 +2,65 @@ package com.mirea.nabiulingb.gamecatalog.presentation.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.mirea.nabiulingb.data.repositories.AuthRepositoryImpl;
 import com.mirea.nabiulingb.gamecatalog.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AuthRepositoryImpl authRepository;
+    private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        authRepository = new AuthRepositoryImpl(this);
+        AuthRepositoryImpl authRepository = new AuthRepositoryImpl(this);
         if (!authRepository.isUserLoggedIn()) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+            startActivity(new Intent(this, LoginActivity.class));
             finish();
             return;
         }
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_drawer_view);
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment_activity_main);
 
         if (navHostFragment != null) {
             NavController navController = navHostFragment.getNavController();
-            NavigationUI.setupWithNavController(navView, navController);
+
+            mAppBarConfiguration = new AppBarConfiguration.Builder(
+                    R.id.gameListFragment, R.id.searchFragment, R.id.profileFragment)
+                    .setOpenableLayout(drawer)
+                    .build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+            NavigationUI.setupWithNavController(navigationView, navController);
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                    || super.onSupportNavigateUp();
+        }
+        return super.onSupportNavigateUp();
     }
 }
